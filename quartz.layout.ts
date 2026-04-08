50214d0a79524ab049d265757465be4781be11b0
liuwei <pseudoway@163.com>
2026-04-08 20:58:10 +0800

    first commit

---
+++ quartz.layout.ts
@@ -0,0 +1,88 @@
+import { PageLayout, SharedLayout } from "./quartz/cfg"
+import * as Component from "./quartz/components"
+
+// components shared across all pages
+export const sharedPageComponents: SharedLayout = {
+  head: Component.Head(),
+  header: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        repo: 'HushWay/blog',
+        repoId: 'R_kgDOQ8xrjA',
+        category: 'Announcements',
+        categoryId: 'DIC_kwDOQ8xrjM4C1JYZ',
+        mapping: 'title',
+        strict: false,
+        reactionsEnabled: '1',
+        inputPosition: 'top',
+      }
+    }),
+  ],
+  footer: Component.Footer({
+    links: {
+      // GitHub: "https://github.com/jackyzha0/quartz",
+      // "Discord Community": "https://discord.gg/cRFFHYye7t",
+    },
+  }),
+}
+
+// components for pages that display a single page (e.g. a single note)
+export const defaultContentPageLayout: PageLayout = {
+  beforeBody: [
+    Component.ConditionalRender({
+      component: Component.Breadcrumbs(),
+      condition: (page) => page.fileData.slug !== "index",
+    }),
+    Component.ArticleTitle(),
+    Component.ContentMeta({ showReadingTime: false }),
+    Component.TagList(),
+  ],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+        // { Component: Component.ReaderMode() },
+      ],
+    }),
+    Component.Explorer({
+      title: "博客",
+      folderDefaultState: "open",
+      folderClickBehavior: "link",
+      useSavedState: false,
+    }),
+  ],
+  right: [
+    Component.TableOfContents()
+    // Component.Graph(),
+    // Component.DesktopOnly(Component.TableOfContents()),
+    // Component.Backlinks(),
+  ],
+}
+
+// components for pages that display lists of pages  (e.g. tags or folders)
+export const defaultListPageLayout: PageLayout = {
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta({ showReadingTime: false })],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+      ],
+    }),
+    Component.Explorer(),
+  ],
+  right: [],
+}
